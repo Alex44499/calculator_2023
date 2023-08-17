a9f29bce99c7198d81bb3adebe3ea2389743ffe2
sizonyuk.a@gmail.com <sizonyuk.a@gmail.com>
2023-08-17 14:26:00 +0300

    Add function operation for AC button

--- main.js
+++ main.js
@@ -8,68 +8,87 @@ const numberButtons = document.querySelectorAll('.number');       // Buttons for
 const operationButtons = document.querySelectorAll('.operation'); // Buttons for mathematical operations
 const displayResult = document.querySelector('.current-value');   // Display area for showing user input and results
 const equalsButton = document.querySelector('.equal');           // Button to trigger the calculation
+const cleaningButton = document.querySelector('.AC');    		 // Button to clean the display
 
 // Function to handle user's number input
 function handleNumberInput() {
-    numberButtons.forEach((button) => {
-        button.addEventListener('click', () => {
-            // If no operator is chosen yet, append to the first input value
-            if (!chosenOperator) {
-                firstInputValue += button.textContent;
-                displayResult.textContent = firstInputValue;
-            } 
-            // If an operator is chosen, append to the second input value
-            else {
-                secondInputValue += button.textContent;
-                displayResult.textContent = firstInputValue + ' ' + chosenOperator + ' ' + secondInputValue;
-            }
-        });
-    });
+	numberButtons.forEach((button) => {
+		button.addEventListener('click', () => {
+			// If no operator is chosen yet, append to the first input value
+			if (!chosenOperator) {
+				firstInputValue += button.textContent;
+				displayResult.textContent = firstInputValue;
+			}
+			// If an operator is chosen, append to the second input value
+			else {
+				secondInputValue += button.textContent;
+				displayResult.textContent = firstInputValue + ' ' + chosenOperator + ' ' + secondInputValue;
+			}
+		});
+	});
 }
 
 // Function to handle user's choice of mathematical operation
 function handleOperatorSelection() {
-    operationButtons.forEach((button) => {
-        button.addEventListener('click', () => {
-            // If an operator hasn't been chosen and there's a first input value
-            if (!chosenOperator && firstInputValue) {
-                chosenOperator = button.textContent;
-                displayResult.textContent = firstInputValue + ' ' + chosenOperator;
-            }
-        });
-    });
+	operationButtons.forEach((button) => {
+		button.addEventListener('click', () => {
+			// If an operator hasn't been chosen and there's a first input value
+			if (!chosenOperator && firstInputValue) {
+				chosenOperator = button.textContent;
+				displayResult.textContent = firstInputValue + ' ' + chosenOperator;
+			}
+		});
+	});
 }
 
 // Function to perform the mathematical operation based on user's input
 function performCalculation(firstNumber, mathOperation, secondNumber) {
-    switch (mathOperation) {
-        case '+':
-            return parseFloat(firstNumber) + parseFloat(secondNumber);
-        case '-':
-            return parseFloat(firstNumber) - parseFloat(secondNumber);
-        case 'x':
-            return parseFloat(firstNumber) * parseFloat(secondNumber);
-        case '/':
-            return parseFloat(firstNumber) / parseFloat(secondNumber);
-        default:
-            return "Invalid operator";
-    }
+	switch (mathOperation) {
+		case '+':
+			return parseFloat(firstNumber) + parseFloat(secondNumber);
+		case '-':
+			return parseFloat(firstNumber) - parseFloat(secondNumber);
+		case 'x':
+			return parseFloat(firstNumber) * parseFloat(secondNumber);
+		case '/':
+			return parseFloat(firstNumber) / parseFloat(secondNumber);
+		default:
+			return "Invalid operator";
+	}
 }
 
 // Function to handle the equal button click and display the result
 function handleEqualsOperation() {
-    equalsButton.addEventListener('click', () => {
-        let result = performCalculation(firstInputValue, chosenOperator, secondInputValue);
-        displayResult.textContent = result;
-		  firstInputValue = '';
-		  secondInputValue = '';
-        chosenOperator = '';
+	equalsButton.addEventListener('click', () => {
+		let result = performCalculation(firstInputValue, chosenOperator, secondInputValue);
+		displayResult.textContent = result;
+		resetCalculator()
 
 
-    });
+	});
 }
 
+
+// Function to handle AC button click and clean the display
+function handleACbutton() {
+	cleaningButton.addEventListener('click', () => {
+		displayResult.textContent = 0
+		resetCalculator()
+	})
+
+}
+
+// Function to reset the calculator's state
+function resetCalculator() {
+	firstInputValue = '';
+	secondInputValue = '';
+	chosenOperator = '';
+}
+
+
 // Initialize event listeners
+resetCalculator()
+handleACbutton();
 handleEqualsOperation();
 handleOperatorSelection();
 handleNumberInput();
